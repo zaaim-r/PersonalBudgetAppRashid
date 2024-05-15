e6b21b171bd9d5c3489b3810d43d077912818aa7
zaaim-r <zaaims@gmail.com>
2024-05-15 14:46:40 -0400

    First commit (set up React, database, etc.)

---
+++ backend/App.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors');
+const { db } = require('./db/db');
+const{readdirSync} = require('fs')
+const app = express()
+
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+app.use(express.json)
+app.use(cors())
+
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+const server = () => {
+    db()
+    app.listen(PORT, () => {
+        console.log('listening to port:', PORT)
+    })
+}
+
+server();
